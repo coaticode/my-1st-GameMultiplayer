822e7c6ef66d7f0a4e3fa68362f74c4354b8dd0d
coati code <sbatis7a@gmail.com>
2024-05-13 19:55:30 +0000

    criando e separando em modulos: render, game, keyboard listener.

---
+++ playground/firstRelease/keyboard-listener.js
@@ -0,0 +1,35 @@
+export default function createKeyboardListener(document) {
+
+    const state = {
+        observers: []
+    }
+
+    function subscribe(observeFunction){
+        state.observers.push(observeFunction)
+
+    }
+    function notifyAll(command) {
+        console.log('keyboardListener -> Notifying ${state.observers.length} observers')
+        
+        for(const observeFunction of state.observers){
+            observeFunction(command)
+        }
+    }
+
+    document.addEventListener('keydown', handKeydown)
+
+    function handKeydown(event) {
+        const keyPressed = event.key
+
+        const command = {
+            playerId: 'player1',
+            keyPressed
+        }
+        notifyAll(command)
+ 
+    }
+
+    return {
+        subscribe
+    }
+}
