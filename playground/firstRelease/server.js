abbe8b0945934725b06fc0fc862dc8eaeff22478
coati code <sbatis7a@gmail.com>
2024-05-15 22:07:05 +0000

    erro: import socketio

--- playground/firstRelease/server.js
+++ playground/firstRelease/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import http from 'http'
 import createGame from './public/game.js'
-import socktio from 'socket.io'
+import socketio from 'socket.io'
 
 const app = express()
 const server = http.createServer(app)
@@ -15,9 +15,11 @@ game.addFruits({fruitId: 'fruit1', fruitX: 5, fruitY: 6})
 
 console.log(game.state)
 
-sockets.on('connection', (socket)=>{
+sockets.on('connection', (socket) => {
     const playerId = socket.id
     console.log('> Player connected on Server With id: ${playerId}')
+
+    socket.emit('setup', game.state)
 })
 
 server.listen(3000, () =>{
