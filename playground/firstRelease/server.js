00633623d493584fd902526ca86d7257744851c1
coati code <sbatis7a@gmail.com>
2024-05-15 01:28:13 +0000

    instalando e configurando socket.io

--- playground/firstRelease/server.js
+++ playground/firstRelease/server.js
@@ -1,10 +1,24 @@
 import express from 'express'
 import http from 'http'
+import createGame from './public/game.js'
+import socktio from 'socket.io'
 
 const app = express()
 const server = http.createServer(app)
+const sockets = socketio(server)
 
 app.use(express.static('public'))
+
+const game = createGame()
+game.addPlayer({playerId: 'player1', playerX: 0, playerY: 1})
+game.addFruits({fruitId: 'fruit1', fruitX: 5, fruitY: 6})
+
+console.log(game.state)
+
+sockets.on('connection', (socket)=>{
+    const playerId = socket.id
+    console.log('> Player connected on Server With id: ${playerId}')
+})
 
 server.listen(3000, () =>{
     console.log('> Server listening on port 3000')
